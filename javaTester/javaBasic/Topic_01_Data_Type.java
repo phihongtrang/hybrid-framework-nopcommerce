dc640644f6548439ae76bb185b81c33ea025d8f1
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-11-24 23:15:51 +0700

    update

--- javaTester/javaBasic/Topic_01_Data_Type.java
+++ javaTester/javaBasic/Topic_01_Data_Type.java
@@ -1,5 +1,27 @@
 package javaBasic;
 
 public class Topic_01_Data_Type {
+	static int studentNumber;
+	static boolean statusValue;
+	static final String browserName = "Chrome";
+	
+	String studentName ="Automation FC";
+	
+	public static void main(String[] args) {
+		System.out.println(studentNumber);
+		System.out.println(statusValue);
+		
+	}
+	
+	//Getter: getCurrentUrl/ getTitle/ getText/ getAttribute/ getCssValue/ getSize/ getLocation/ getPosition...
+	public String getStudentName() {
+		return this.studentName;
+		
+	}
+	
+	//Setter: click /sendkey/ clear/ select/ back/ forward/ refresh/ get...
+	public void setStudentName(String stdName) {
+		this.studentName = stdName;
+	}
 
 }
