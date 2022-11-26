1ef327653c84c6926bad8e5d0833fba2e0609b92
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-11-26 22:53:36 +0700

    update

---
+++ javaTester/javaBasic/Topic_04_Operator.java
@@ -0,0 +1,28 @@
+package javaBasic;
+
+public class Topic_04_Operator {
+	
+	public static  void main(String[] args) {
+		int number = 10;
+		number += 5;
+		System.out.println(number);
+		
+		// 15/3 = 3
+		System.out.println(number / 5);
+		
+		//15%6 2 dư 3
+		System.out.println(number % 7);
+		
+		// In number ra trước
+		System.out.println(number++);
+		
+		// Tăng number lên trước
+		System.out.println(++number);
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println(i);
+			
+		}
+
+	}
+}
