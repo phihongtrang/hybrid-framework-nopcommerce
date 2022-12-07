4397ac43293557f7c6efba1583f94b5e7a92facd
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-12-07 22:59:59 +0700

    update

---
+++ javaTester/eclipseTips/Topic_03_Getter_Setter.java
@@ -0,0 +1,14 @@
+package eclipseTips;
+
+public class Topic_03_Getter_Setter {
+	private String carName;
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+}
