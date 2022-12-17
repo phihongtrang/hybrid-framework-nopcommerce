3041f858a8e938f8563af185ad79461e77f5187c
phihongtrang <phi.hong.trang.1996@gmail.com>
2022-12-17 14:42:07 +0700

    update

--- javaTester/javaBasic/Topic_12_Exercise.java
+++ javaTester/javaBasic/Topic_12_Exercise.java
@@ -3,20 +3,45 @@ package javaBasic;
 import org.testng.annotations.Test;
 
 public class Topic_12_Exercise {
-	String courseName = "Automation Testing Advanced";
+	String courseName = "AuTomation TesTing AdVanced 2022";
 
 	@Test
 	public void TC_01() {
 		char courseNameArr[] = courseName.toCharArray();
-		int count = 0;
+		int countUpper = 0;
+		int countLowser = 0;
+		int countNumber = 0;
 		for (char character : courseNameArr) {
 			if (character <= 'Z' && character >= 'A') {
-				count++;
+				countUpper++;
+			}
+			if (character <= 'z' && character >= 'a') {
+				countLowser++;
+
+			}
+
+			if (character <= '9' && character >= '0') {
+				countNumber++;
 
 			}
 
 		}
-		System.out.println(" Sum of uppercase =" + count);
+		System.out.println(" Sum of uppercase =" + countUpper);
+		System.out.println(" Sum of uppercase =" + countLowser);
+		System.out.println(" Sum of uppercase =" + countNumber);
+	}
+
+	@Test
+	public void TC_02() {
+		char courseNameArr[] = courseName.toCharArray();
+		int count = 0;
+		for (char c : courseNameArr) {
+			if (c == 'a') {
+				count++;
+
+			}
+			System.out.println(count);
+		}
 	}
 
 }
